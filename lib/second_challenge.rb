37056115f6604eae154d4feee66e4f2ad25046e2
Ian Pollack <ianjpollack@gmail.com>
2018-03-27 22:51:49 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,7 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
-  #code your solution here!
-
-  
-
+  nestedList = groceries.values
+  finalList = nestedList.flatten
+  finalList
 end
